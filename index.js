e16c94099da299c50815e82ed26a66ce4b18effb
Akarsh Satija <akarsh.satija@gmail.com>
2015-02-22 19:50:07 +0530

    Updating Dependencies

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ var config = require('./config');
 
 var db = mongoose.connect(config.db.uri, config.db.options, function(err) {
     if (err) {
-        console.error(chalk.red('Could not connect to MongoDB!'));
-        console.log(chalk.red(err));
+        console.error('Could not connect to MongoDB!');
+        console.log(err);
     }
 });
 
